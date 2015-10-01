da14f8b58d72756c7d8be154148f891797f8fbae
Winthrop Gillis <wgillis@vpn-offcampus-168-122-67-113.bu.edu>
2015-10-01 08:14:54 -0400

    Basic mac startup script

---
+++ mac.sh
@@ -0,0 +1,12 @@
+export CLICOLOR=1
+export LSCOLORS=GxFxCxDxBxegedabagaced
+
+# added by Anaconda3 2.3.0 installer
+export PATH="/Users/wgillis/anaconda/bin:$PATH:~/pyrun:~/pyimport"
+
+export work="wgillis@cumm024-0b02-dhcp-180.bu.edu"
+
+alias matlab="/Applications/MATLAB_R2015a.app/bin/matlab"
+alias matterm="matlab -nodesktop -nosplash"
+
+echo "Welcome Winthrop"
