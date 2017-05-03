197b3901fc894567a439341d96f08b59a145eb13
wingillis <win.gillis@gmail.com>
2017-05-03 10:38:22 -0400

    Added orchid to repo and added harvard-specific endpoints to mac.sh

--- mac.sh
+++ mac.sh
@@ -2,11 +2,20 @@ export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced
 
 # added by Anaconda3 2.3.0 installer
-export PATH="/Users/wgillis/anaconda/bin:$PATH:~/pyrun:~/pyimport"
+export PATH="/Users/wgillis/anaconda/bin:$PATH:~/pyrun:~/pyimport:~/Documents/MATLAB/birds/py"
 
 export work="wgillis@cumm024-0b02-dhcp-180.bu.edu"
-
-alias matlab="/Applications/MATLAB_R2015a.app/bin/matlab"
+export pybirds="/Users/wgillis/Documents/MATLAB/birds/py"
+alias matlab="/Applications/MATLAB_R2016b.app/bin/matlab"
 alias matterm="matlab -nodesktop -nosplash"
 
 echo "Welcome Winthrop"
+
+export neurobioServer="wg41@transfer.orchestra.med.harvard.edu:/files/Neurobio/DattaLab/win/"
+export groupsFolder="wg41@transfer.orchestra.med.harvard.edu:/groups/datta/win/"
+alias mountneurobio="sshfs ${neurobioServer} neuro/"
+alias mountgroup="sshfs ${groupsFolder} neuro/"
+# make bash use vi keybindings
+set -o vi
+
+echo "Vi keybindings now loaded!"
