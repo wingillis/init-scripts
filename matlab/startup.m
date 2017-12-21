5a92c970148d979c43760f47b337bd1d38444fe2
Winthrop Gillis <win.gillis@gmail.com>
2017-12-21 15:37:03 -0500

    add matlab startup script

---
+++ matlab/startup.m
@@ -0,0 +1,10 @@
+
+addpath(genpath('/home/wg41/code/d1d2_paper'));
+addpath(genpath('/home/wg41/code/grin-analysis'));
+addpath(genpath('/home/wg41/code/cnmfe_old'));
+addpath(genpath('/home/wg41/code/cbrewer'));
+addpath(genpath('/home/wg41/code/kinect-extraction'));
+addpath(genpath('/home/wg41/code/hifiber'));
+addpath(genpath('/home/wg41/code/schfigure'));
+addpath(genpath('/home/wg41/code/nacc-photometry'));
+addpath(genpath('/home/wg41/code/phanalysis'));
