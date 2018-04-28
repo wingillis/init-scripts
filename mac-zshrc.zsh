d3d96731b1df78adb16e14ae36cc1c686855b772
wingillis <win.gillis@gmail.com>
2018-04-27 22:31:00 -0400

    Added personal syntax highlighting

--- mac-zshrc.zsh
+++ mac-zshrc.zsh
@@ -105,6 +105,12 @@ function commit_diff {
 	git --no-pager diff --stat ${1}^1 ${1}
 }
 
+ZSH_HIGHLIGHT_STYLES[command]='fg=cyan'
+ZSH_HIGHLIGHT_STYLES[alias]='fg=cyan'
+ZSH_HIGHLIGHT_STYLES[precommand]='fg=cyan'
+ZSH_HIGHLIGHT_STYLES[builtin]='fg=cyan'
+
+
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/wgillis/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/wgillis/google-cloud-sdk/path.zsh.inc'; fi
 
