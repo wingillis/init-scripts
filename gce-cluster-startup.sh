4f07ac50c14a56d2d539118c547097d42f2888f8
wingillis <win.gillis@gmail.com>
2018-05-09 11:59:23 -0400

    startup script for gce

---
+++ gce-cluster-startup.sh
@@ -0,0 +1,27 @@
+
+mkdir dev
+mkdir data
+
+# download miniconda
+curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o "$HOME/miniconda3_latest.sh"
+
+cd dev
+
+git clone https://github.com/dattalab/moseq2-extract.git
+git clone https://github.com/dattalab/moseq2-pca.git
+git clone https://github.com/dattalab/moseq2-model.git
+git clone https://github.com/dattalab/moseq2-batch.git
+
+cd $HOME
+bash miniconda3_latest.sh -b -p $HOME/miniconda3
+
+echo "export PATH=$HOME/miniconda3/bin:\$PATH" >> .bashrc
+
+source $HOME/.bashrc
+
+pip install -e dev/moseq2-extract
+pip install -e dev/moseq2-pca
+pip install -e dev/moseq2-model --process-dependency-links
+pip install -e dev/moseq2-batch
+
+gsutil -m cp -r gs://datta-shared-data/realsense $HOME/data/
