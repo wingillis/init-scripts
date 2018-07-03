dabbec2bdd245fc83f3aba1b41cb619d27bad36a
wingillis <win.gillis@gmail.com>
2018-07-03 14:30:34 -0400

    Added an sbatch script for installation

--- gce-cluster-startup.sh
+++ gce-cluster-startup.sh
@@ -7,11 +7,11 @@ curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o "$
 
 cd dev
 
-git clone https://github.com/dattalab/moseq2-extract.git
-git clone https://github.com/dattalab/moseq2-pca.git
-git clone https://github.com/dattalab/moseq2-model.git
-git clone https://github.com/dattalab/moseq2-batch.git
-git clone https://github.com/dattalab/moseq2-viz.git
+git clone https://wingillis@github.com/dattalab/moseq2-extract.git
+git clone https://wingillis@github.com/dattalab/moseq2-pca.git
+git clone https://wingillis@github.com/dattalab/moseq2-model.git
+git clone https://wingillis@github.com/dattalab/moseq2-batch.git
+git clone https://wingillis@github.com/dattalab/moseq2-viz.git
 
 cd $HOME
 bash miniconda3_latest.sh -b -p $HOME/miniconda3
@@ -25,8 +25,6 @@ pip install -e dev/moseq2-pca
 pip install -e dev/moseq2-model --process-dependency-links
 pip install -e dev/moseq2-batch
 pip install -e dev/moseq2-viz
-
-gsutil -m cp -r gs://datta-shared-data/cables-2 $HOME/data/
 
 git config --global user.email "win.gillis@gmail.com"
 git config --global user.name "Winthrop Gillis"
@@ -43,3 +41,5 @@ ln -s $HOME/init-scripts/gce-zshrc.zsh $HOME/.zshrc
 echo "exec zsh" >> $HOME/.bash_profile
 
 . $HOME/.bash_profile
+
+sbatch -t 12:00:00 -n 1 -c 8 --mem=30G --wrap "gsutil -m cp -r gs://datta-shared-data/cables-2 $HOME/data/"
