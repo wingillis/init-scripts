3f0f0a8198bcc1d37095a5d5e241b12cd2391c9f
wingillis <win.gillis@gmail.com>
2018-07-22 12:27:45 -0400

    added dropbox paper chrome app alias

--- mac-zshrc.zsh
+++ mac-zshrc.zsh
@@ -100,6 +100,7 @@ export GOPATH="${HOME}/dev/go"
 
 alias curcommit="git rev-parse HEAD | cut -c1-7"
 alias copycommit="curcommit | pbcopy"
+alias db-paper="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --app=\"https://paper.dropbox.com/\""
 
 function commit_diff {
 	git --no-pager diff --stat ${1}^1 ${1}
