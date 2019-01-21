0d4a880f581503f6efb9770b55f1bf11938a5505
wingillis <win.gillis@gmail.com>
2019-01-20 19:44:51 -0500

    An ignore file and an automatic vim pluginInstall

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ mkdir -p $HOME/.vim/ftplugin
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
 ln -s $HOME/dev/init-scripts/vim-ft-exts/*.vim $HOME/.vim/ftplugin/
+
+ln -s $HOME/dev/init-scripts/.vimrc $HOME/.vimrc
+
+vim --cmd "PluginInstall" -c "qa"
