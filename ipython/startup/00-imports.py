362611e9977c02e913a5c72ecbd3a72f307010b1
wingillis <win.gillis@gmail.com>
2019-08-05 16:09:40 +0000

    added import

--- ipython/startup/00-imports.py
+++ ipython/startup/00-imports.py
@@ -8,8 +8,10 @@ import numpy as np
 
 # utils
 import os
+import joblib
 from tqdm import tqdm_notebook
 from pathlib import Path
+from os.path import join, basename, dirname
 from dls_analysis.viz.util import saver_factory, fg, subplots
 
 plt.style.use(['dark_background', 'win-dark'])
